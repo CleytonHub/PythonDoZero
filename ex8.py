9e741157147188f586ab38e030b75a50666e0aec
CleytonHub <cleyton.automacao@gmail.com>
2025-05-18 23:23:07 -0300

    Aprendendo mais operações matemáticas

---
+++ ex8.py
@@ -0,0 +1,14 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+a = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {a}')
+s = n1 - n2
+print(f'A subtração entre {n1} e {n2} é igual a {s}')
+m = n1 * n2
+print(f'A multiplicação entre {n1} e {n2} é igual a {m}') 
+d = n1/n2
+print(f'A divisão entre {n1} e {n2} é igual a {d}') 
+di = n1//n2
+print(f'A divisão inteira entre {n1} e {n2} é igual a {di}') 
+e = n1**n2
+print(f'A potenciação entre {n1} e {n2} é igual a {e}') 
